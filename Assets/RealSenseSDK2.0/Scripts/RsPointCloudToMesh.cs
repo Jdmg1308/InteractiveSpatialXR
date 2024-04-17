0150b67c0ec95308a44988d2489e49b644ab51af
Juan Diego <juandiegomendezgutierrez@gmail.com>
2024-04-16 19:04:13 -0500

    it looks neat af rn

--- Assets/RealSenseSDK2.0/Scripts/RsPointCloudToMesh.cs
+++ Assets/RealSenseSDK2.0/Scripts/RsPointCloudToMesh.cs
@@ -26,20 +26,15 @@ using System.Net.NetworkInformation;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class RsPointCloudToMesh : MonoBehaviour
 {
-    private const float BOX_DIST_THRESHOLD = 0.3f;
-    private const float PLANE_DIST_THRESHOLD = 2;
-    private const int MIN_POINTS_FOR_SHAPE = 100; // Adjust as needed
-    private int key_count = 0;
+    private const int MIN_POINTS_FOR_SHAPE = 50; // Adjust as needed
     public Material hullMaterial;
     private Mesh mesh;
     private Texture2D uvmap;
     private FrameQueue q;
-    private Dictionary<int, GameObject> GameObjReference = new Dictionary<int, GameObject>();
-    private Dictionary<int, Vector3[]> VerticesReference = new Dictionary<int, Vector3[]>();
-    private Dictionary<int, Vector3> PlaneNormalReference = new Dictionary<int, Vector3>();
     // Create a dictionary to store voxel counts
     private Dictionary<Vector3Int, int> voxelCounts = new Dictionary<Vector3Int, int>();
-        private Dictionary<Vector3Int, GameObject> voxelRef = new Dictionary<Vector3Int, GameObject>();
+    // Create a dictionary to store voxel objects
+    private Dictionary<Vector3Int, GameObject> voxelRef = new Dictionary<Vector3Int, GameObject>();
     private readonly int[] cubeTriangles = new int[] 
     {
         // Front face
@@ -191,7 +186,6 @@ public class RsPointCloudToMesh : MonoBehaviour
         }
     }
 
-
     protected void LateUpdate()
     {
         if (q != null)
@@ -244,8 +238,8 @@ public class RsPointCloudToMesh : MonoBehaviour
 
     private void Helper(int value)
     {
-        // Define your voxel size
-        float voxelSize = 0.2f; // Example size, adjust as needed
+        // voxel size
+        float voxelSize = 0.13f;
 
         // Iterate over all points and count points in each voxel
         if (value % 3 == 0) {
@@ -257,8 +251,6 @@ public class RsPointCloudToMesh : MonoBehaviour
                     Mathf.FloorToInt(point.z / voxelSize)
                 );
 
-                // Increment the count for this voxel
-                // if (!InsideOfVoxel(voxelPosition, point, voxelSize)) {
                 if (!voxelCounts.ContainsKey(voxelPosition))
                 {
                     voxelCounts[voxelPosition] = 1;
@@ -267,279 +259,41 @@ public class RsPointCloudToMesh : MonoBehaviour
                 {
                     voxelCounts[voxelPosition]++;
                 }
-                // }
             }
         }
 
-        // Iterate over voxel counts and create shapes where necessary
-        foreach (var kvp in voxelCounts)
+        // Iterate over voxel counts and create/delete boxes where necessary
+        foreach (var vox_pos_key in new List<Vector3Int>(voxelCounts.Keys))
         {
-            Vector3Int voxelPosition = kvp.Key;
-            int pointCount = kvp.Value;
-
-            Vector3 voxelMin = new Vector3(
-                    (voxelPosition.x) * voxelSize,
-                    (voxelPosition.y) * voxelSize,
-                    (voxelPosition.z) * voxelSize
-                );
-            Vector3 voxelMax = new Vector3(
-                    (voxelPosition.x + 1) * voxelSize,
-                    (voxelPosition.y + 1) * voxelSize,
-                    (voxelPosition.z + 1) * voxelSize
-                );
-
-            Vector3[] points = CreateNewBox(voxelMin, voxelMax);
+            if (voxelCounts[vox_pos_key] > 1) {
+                voxelCounts[vox_pos_key] -= voxelCounts[vox_pos_key] / (MIN_POINTS_FOR_SHAPE / 10);
+            } else { voxelCounts[vox_pos_key]++; }
+            int pointCount = voxelCounts[vox_pos_key];
 
             if (pointCount >= MIN_POINTS_FOR_SHAPE)
             {
-                // Calculate the center of the voxel
-                // Vector3 voxelCenter = new Vector3(
-                //     (voxelPosition.x + 0.5f) * voxelSize,
-                //     (voxelPosition.y + 0.5f) * voxelSize,
-                //     (voxelPosition.z + 0.5f) * voxelSize
-                // );
-                if (!voxelRef.ContainsKey(voxelPosition)) {            
-                    // You can now create a shape (e.g., box) at voxelCenter
-                    // Example: CreateNewBox(voxelCenter, ...);
-                    SetPointsReference(key_count, points);
-                    PlaneNormalReference[key_count] = new Vector3(0, 0, -1);
-                    GameObject gOref = CreateGameObjs(key_count, points);
-                    key_count++;
-                    voxelRef[voxelPosition] = gOref;
+                Vector3 voxelMin = new Vector3(
+                        vox_pos_key.x * voxelSize,
+                        vox_pos_key.y * voxelSize,
+                        vox_pos_key.z * voxelSize
+                    );
+                Vector3 voxelMax = new Vector3(
+                        (vox_pos_key.x + 1) * voxelSize,
+                        (vox_pos_key.y + 1) * voxelSize,
+                        (vox_pos_key.z + 1) * voxelSize
+                    );
+                Vector3[] points = CreateNewBox(voxelMin, voxelMax);
+                if (!voxelRef.ContainsKey(vox_pos_key)) {
+                    voxelRef[vox_pos_key] = CreateGameObjs(points);
                 }
             } else {
-                // if (VerticesReference.ContainsValue(points)) {            
-                //     // You can now create a shape (e.g., box) at voxelCenter
-                //     // Example: CreateNewBox(voxelCenter, ...);
-                //     // VerticesReference.Remove(VerticesReference.);
-                //     // PlaneNormalReference[key_count] = new Vector3(0, 0, -1);
-                //     // CreateGameObjs(key_count, points);
-                //     // key_count++;
-                // }
-            }
-        }
-    }
-
-    // use the voxels
-
-    private void HelperHelper()
-    {
-        float MaxVal = 3;
-        float MinVal = 0.0f;
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            Vector3 point = vertices[i];
-            if (point != Vector3.zero &&
-                Math.Abs(point.x) < MaxVal && Math.Abs(point.y) < MaxVal && Math.Abs(point.z) < MaxVal)
-                // Math.Abs(point.x) < MaxVal && Math.Abs(point.y) < MaxVal && Math.Abs(point.z) < MaxVal &&
-                // Math.Abs(point.x) > MinVal && Math.Abs(point.y) > MinVal && Math.Abs(point.z) > MinVal)
-            {
-                int key_to_update = -2; // -2 means do not do anything
-                if (usePointDecider(point, ref key_to_update)) { // update set
-                    UpdateSet(key_to_update, point);
-
-                } else if (key_to_update == -1) { // create a new set
-                    Vector3[] points = CreateNewBox(point, point);
-                    SetPointsReference(key_count, points);
-                    PlaneNormalReference[key_count] = new Vector3(0, 0, -1);
-                    CreateGameObjs(key_count, points);
-                    key_count++;
+                if (voxelRef.ContainsKey(vox_pos_key)) {
+                    Destroy(voxelRef[vox_pos_key]);
+                    voxelRef.Remove(vox_pos_key);
                 }
             }
         }
     }
-
-    public bool usePointDecider(Vector3 point, ref int boxToUpdateKey) {
-        boxToUpdateKey = -2;
-        if (VerticesReference.Keys.Count == 0)
-        {
-            boxToUpdateKey = -1;
-            return false;
-        }
-        float minDist = float.MaxValue;
-        int minBox = 0;
-
-        foreach (int i in VerticesReference.Keys)
-        {
-            if (InsideOfBox(GetPointsReference(i), point)) { return false; }
-            else
-            {
-                float dist = ClosestPointDistance(point, GetPointsReference(i));
-                if (dist <= minDist)
-                {
-                    minDist = dist;
-                    minBox = i;
-                }
-            }
-        }
-        if (minDist <= BOX_DIST_THRESHOLD)
-        {
-            if (DistanceToPlane(minBox, point) < PLANE_DIST_THRESHOLD) {
-                boxToUpdateKey = minBox;
-                return true;
-            } 
-            // else {
-            //     boxToUpdateKey = -1;
-            // }
-        }
-        return false;
-    }
-
-    private float DistanceToPlane(int key, Vector3 point)
-    {
-        Vector3 planeNormal = PlaneNormalReference[key];
-        Vector3[] points = VerticesReference[key];
-        Vector3 pointToPlane = point - points[6];
-        float dotProduct = Vector3.Dot(pointToPlane, planeNormal);
-        return Mathf.Abs(dotProduct) / planeNormal.magnitude;
-    }
-
-    private float ClosestPointDistance(Vector3 p, Vector3[] bpx)
-    {
-        float minDist = float.MaxValue;
-        foreach (Vector3 vertex in bpx)
-        {
-            float dist = Vector3.Distance(vertex, p);
-            minDist = Mathf.Min(dist, minDist);
-        }
-        return minDist;
-    }
-
-    private bool InsideOfBox(Vector3[] box, Vector3 p)
-    {
-        Vector3 min = box[0];
-        Vector3 max = box[6];
-        return !(
-            Math.Abs(p.x) < Math.Abs(min.x) || 
-            Math.Abs(p.x) > Math.Abs(max.x) || 
-            Math.Abs(p.y) < Math.Abs(min.y) || 
-            Math.Abs(p.y) > Math.Abs(max.y) || 
-            Math.Abs(p.z) < Math.Abs(min.z) || 
-            Math.Abs(p.z) > Math.Abs(max.z)
-        );
-    }
-
-    // private bool InsideOfVoxel(Vector3Int v, Vector3 p, float s)
-    // {
-    //     Vector3 min = new Vector3(
-    //                 (v.x) * s,
-    //                 (v.y) * s,
-    //                 (v.z) * s
-    //             );
-    //     Vector3 max = new Vector3(
-    //                 (v.x + 0.5f) * s,
-    //                 (v.y + 0.5f) * s,
-    //                 (v.z + 1) * s
-    //             );
-    //     return !(
-    //         Math.Abs(p.x) < Math.Abs(min.x) || 
-    //         Math.Abs(p.x) > Math.Abs(max.x) || 
-    //         Math.Abs(p.y) < Math.Abs(min.y) || 
-    //         Math.Abs(p.y) > Math.Abs(max.y) || 
-    //         Math.Abs(p.z) < Math.Abs(min.z) || 
-    //         Math.Abs(p.z) > Math.Abs(max.z)
-    //     );
-    // }
-    bool InsideOfVoxel(Vector3Int voxelPosition, Vector3 point, float voxelSize)
-    {
-        Vector3 minBound = new Vector3(voxelPosition.x * voxelSize, voxelPosition.y * voxelSize, voxelPosition.z * voxelSize);
-        Vector3 maxBound = new Vector3((voxelPosition.x + 1) * voxelSize, (voxelPosition.y + 1) * voxelSize, (voxelPosition.z + 1) * voxelSize);
-
-        return point.x >= minBound.x && point.x <= maxBound.x &&
-            point.y >= minBound.y && point.y <= maxBound.y &&
-            point.z >= minBound.z && point.z <= maxBound.z;
-    }
-
-    private void UpdateSet(int key, Vector3 p)
-    {
-        Vector3[] points = VerticesReference[key];
-        if (points == null)
-            return;
-
-        Vector3 min = points[0];
-        Vector3 max = points[6];
-
-        if (p.x < min.x || p.x > max.x || p.y < min.y || p.y > max.y || p.z < min.z || p.z > max.z)
-        {
-            min = Vector3.Min(min, p);
-            max = Vector3.Max(max, p);
-            Vector3[] newBox = CreateNewBox(max, min);
-
-            // if (Math.Abs(newBox[6].z - newBox[0].z) >= 0.1)
-            // {
-            //     max.z = points[6].z;
-            //     min.z = points[0].z;
-            //     RotationMethod(key, newBox, GetGameObjectReference(key), p);
-            // }
-            // else
-            // {
-                UpdateMesh(newBox, GetGameObjectReference(key));
-                SetPointsReference(key, newBox);
-                UpdatePlane(key, Quaternion.identity);
-            // }
-        }
-    }
-
-    private void RotationMethod(int key, Vector3[] points, GameObject obj, Vector3 p)
-    {
-        Vector3[] OG = GetPointsReference(key);
-        // If the distance between the plane and the point is too big do nothing
-        // Vector3 minPoint = points[0];
-        // Vector3 maxPoint = points[0];
-
-        // foreach (Vector3 point in points)
-        // {
-        //     minPoint = Vector3.Min(minPoint, point);
-        //     maxPoint = Vector3.Max(maxPoint, point);
-        // we first update the plane based on points beyond threshold
-        // if the box is too big we apply a rotation and make it slimer
-        // }
-        Vector3 minPoint = OG[0];
-        Vector3 maxPoint = OG[6];
-
-        // Get the parent GameObject of the childObject
-        GameObject parentObject = obj.transform.parent.gameObject;
-
-        Vector3 center = boxCenter(OG);
-        // Vector3 direction = maxPoint - minPoint;
-        Vector3 direction = p - center;
-
-        // Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
-
-        Quaternion rotation = Quaternion.LookRotation(new Vector3(0.1f, 0.1f, 0), Vector3.up);
-
-        // position - center after rot
-        parentObject.transform.rotation = rotation;
-
-        UpdatePlane(key, rotation);
-    }
-
-    private void UpdatePlane(int key, Quaternion q)
-    {
-        Vector3[] points = VerticesReference[key];
-        if (q == Quaternion.identity)
-        {
-            float width = Mathf.Abs(points[6].x - points[0].x);
-            float height = Mathf.Abs(points[6].y - points[0].y);
-            float depth = Mathf.Abs(points[6].z - points[0].z);
-
-            if (width > height && width > depth)
-                PlaneNormalReference[key] = new Vector3(1, 0, 0);
-            else if (depth > width)
-                PlaneNormalReference[key] = new Vector3(0, 0, 1);
-            else
-                PlaneNormalReference[key] = new Vector3(0, 1, 0);
-        }
-        else
-        {
-            Vector3 normal = PlaneNormalReference[key];
-            Quaternion normalQuaternion = new Quaternion(normal.x, normal.y, normal.z, 0f);
-            Quaternion rotatedNormalQuaternion = q * normalQuaternion;
-            Vector3 rotatedNormal = new Vector3(rotatedNormalQuaternion.x, rotatedNormalQuaternion.y, rotatedNormalQuaternion.z);
-            PlaneNormalReference[key] = rotatedNormal;
-        }
-    }
-
     private Vector3[] CreateNewBox(Vector3 max, Vector3 min)
     {
         Vector3[] points = new Vector3[8];
@@ -553,115 +307,24 @@ public class RsPointCloudToMesh : MonoBehaviour
         points[7] = new Vector3(min.x, max.y, max.z);
         return points;
     }
-
-    private GameObject CreateGameObjs(int key, Vector3[] points)
+    private GameObject CreateGameObjs(Vector3[] points)
     {
         GameObject newGameObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
         Mesh mesh = new Mesh();
         mesh.vertices = points;
         mesh.triangles = cubeTriangles;
+        // mesh.RecalculateNormals();
         newGameObj.GetComponent<MeshFilter>().mesh = mesh;
-        GameObjReference[key] = newGameObj;
-        // Create a new material with the desired color
-        Material boxMaterial = new Material(Shader.Find("Standard"));
-        // boxMaterial.color = Color.blue; // Change the color to whatever you want
         newGameObj.GetComponent<MeshRenderer>().material = hullMaterial;
-        // Create parent GameObject for rotation
-        GameObject parentObject = new GameObject("ParentObject");
-        // Translate its position
-        parentObject.transform.position = boxCenter(points);
-        // Set the parent of childObject to parentObject
-        newGameObj.transform.SetParent(parentObject.transform);
+        Destroy(newGameObj.GetComponent<BoxCollider>());
+        MeshCollider boxCollider = newGameObj.AddComponent<MeshCollider>();
+        // boxCollider.convex = false;
+        boxCollider.convex = true;
         return newGameObj;
     }
-
-    public void UpdateMesh(Vector3[] vertices, GameObject gameObject) 
-    {
-        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
-        if (meshFilter == null) { UnityEngine.Debug.LogError("MeshFilter component not found on the GameObject."); return; }
-
-        Mesh mesh = meshFilter.mesh;
-
-        mesh.Clear();
-        mesh.vertices = vertices;
-        mesh.triangles = cubeTriangles;
-        mesh.RecalculateNormals();
-    }
-
     private void Initialize()
     {
-        PlaneNormalReference.Clear();
-        VerticesReference.Clear();
-        GameObjReference.Clear();
-    }
-
-    // Method to set the GameObj for a given key
-    public void SetGameObjectReference(int key, GameObject value)
-    {
-        if (GameObjReference.ContainsKey(key)) { 
-            // Key already exists, update the value 
-            GameObjReference[key] = value;
-        }
-        else
-        {   // Key doesn't exist, add a new entry
-            GameObjReference.Add(key, value);
-        }
-    }
-    // Method to get the gameObject reference for a given key
-    public GameObject GetGameObjectReference(int key)
-    {
-        GameObject gamObj = null;
-        GameObjReference.TryGetValue(key, out gamObj);
-        return gamObj;
-    }
-    // Method to set the points array  for a given key
-    public void SetPointsReference(int key, Vector3[] value)
-    {
-        if (VerticesReference.ContainsKey(key)) { 
-            // Key already exists, update the value 
-            VerticesReference[key] = value;
-            // Store the current position of the child relative to the parent
-            GameObject thisOne = GetGameObjectReference(key);
-            Vector3 childLocalPosition = thisOne.transform.position;
-            // Update the position of the parent without moving the child
-            GameObject parentObject = thisOne.transform.parent.gameObject;
-            parentObject.transform.position = boxCenter(value);
-            // Reapply the offset to the child's position to maintain its position relative to the parent
-            thisOne.transform.position = childLocalPosition;
-        }
-        else
-        {   // Key doesn't exist, add a new entry
-            VerticesReference.Add(key, value);
-        }
-    }
-    // Method to get the points array reference for a given key
-    public Vector3[] GetPointsReference(int key)
-    {
-        Vector3[] points = null;
-        VerticesReference.TryGetValue(key, out points);
-        return points;
-    }
-
-    public void SetPlaneNormalReference(int key, Vector3 normal)
-    {
-        if (PlaneNormalReference.ContainsKey(key)) { 
-            // Key already exists, update the value 
-            PlaneNormalReference[key] = normal;
-        }
-        else
-        {   // Key doesn't exist, add a new entry
-            PlaneNormalReference.Add(key, normal);
-        }
-    }
-    // Method to get the normal reference for a given key
-    public Vector3 GetPlaneNormalReference(int key)
-    {
-        Vector3 normal;
-        PlaneNormalReference.TryGetValue(key, out normal);
-        return normal;
-    }
-
-    public Vector3 boxCenter(Vector3[] box) {
-        return (box[0] + box[6]) * 0.5f;
+        voxelRef.Clear();
+        voxelCounts.Clear();
     }
 }
